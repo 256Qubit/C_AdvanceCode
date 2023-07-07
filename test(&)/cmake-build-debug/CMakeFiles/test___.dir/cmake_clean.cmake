1cada8495b246f910f0830b7a5a5e7f78e717402
256Qubit <jmortyrick@gmail.com>
2023-07-07 16:24:27 +0800

    test(&) 提交

---
+++ test(&)/cmake-build-debug/CMakeFiles/test___.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test___.dir/main.c.obj"
+  "libtest___.dll.a"
+  "test___.exe"
+  "test___.exe.manifest"
+  "test___.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/test___.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
