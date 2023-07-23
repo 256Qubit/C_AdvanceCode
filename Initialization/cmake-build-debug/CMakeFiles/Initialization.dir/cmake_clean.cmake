8156de3e68b7448a9d37561dd19875bd958ca2ab
256Qubit <jmortyrick@gmail.com>
2023-07-23 15:50:25 +0800

    initialization提交

---
+++ Initialization/cmake-build-debug/CMakeFiles/Initialization.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Initialization.dir/main.c.obj"
+  "Initialization.exe"
+  "Initialization.exe.manifest"
+  "Initialization.pdb"
+  "libInitialization.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Initialization.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Initialization/cmake-build-debug/CMakeFiles/Initialization.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Initialization.
+# This may be replaced when dependencies are built.
