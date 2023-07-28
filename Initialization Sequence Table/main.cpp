6bdcf7adf19118957e8e053d742019be07077c8e
256Qubit <jmortyrick@gmail.com>
2023-07-28 15:36:36 +0800

    Initialization sequence table 提交

--- Initialization Sequence Table/main.cpp
+++ Initialization Sequence Table/main.cpp
@@ -14,14 +14,32 @@ void InitList(SqList &L){
         L.length=0;//顺序表初始长度为0
     }
 }
+bool ListInsert(SqList &L,int i,int e){
+    if(i<1||i>L.length+1){//判断i范围是否有效
+        return false;
+    }
+    if(L.length>=MaxSize){//当前空间已满，不能插入
+        return false;
+    }
+    for(int j=L.length;j>i;j--){//将第i个元素之后往后移
+        L.data[j]=L.data[j-1];
+    }
+    L.data[i-1]=e;//在第i处放置e
+    L.length++;//长度加1
+
+    return true;
+
+}
 
 int main() {
     SqList L;//声明一个顺序表
     InitList(L);//初始化一个顺序表
 
+    ListInsert(L,3,3);
+
 //    for (int i = 0; i < MaxSize; ++i)
-    for (int i = 0; i <L.length; ++i) {
-        printf("data[%d]=%d\n",i,L.data[i]);
-    }
+//    for (int i = 0; i <L.length; ++i) {
+//        printf("data[%d]=%d\n",i,L.data[i]);
+//    }
     return 0;
 }
