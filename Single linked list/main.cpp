a99e2c3f5eab3ad01f0c3f7f0bf7b632bdc9f969
256Qubit <jmortyrick@gmail.com>
2023-08-21 21:10:54 +0800

    Single Linked List(ListInsert) 提交

--- Single linked list/main.cpp
+++ Single linked list/main.cpp
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 //typedef struct LNode{//定义单链表类型结构
 //    int data;//每个结点存放的数据类型
 //    struct LNode *next;//指针指向下一个结点
 //}LNode,*LinkList;
+
 typedef struct LNode LNode;//定义LNode代替struct LNode
 typedef struct LNode *LinkList;//定义一个LinkList表示指向LNode的指针（头指针）
 struct LNode{
@@ -20,9 +23,35 @@ bool Empty(LinkList L){//判断单链表是否为空
         return false;
     }
 }
+
 //bool Empty(LinkList L){
 //    return (L==NULL);
 //}
+bool ListInsert(LinkList &L,int i,int e){
+    if(i<1){
+        return false;
+    }
+    if(i==1){
+        LNode *s=(LNode*) malloc(sizeof(LNode));
+        s->data=e;//先将L写到一边
+        s->next=L;//让s的next指针指向L指针所指向的结点
+        L=s;//将L指针指向结点
+    }
+    LNode *p;
+    int j=1;
+    p=L;
+    while (p!=NULL&&j<i-1){
+        p=p->next;
+        p++;
+    }
+    if(p==NULL){
+        return false;
+    }
+    LNode *s=(LNode *) malloc(sizeof (LNode));
+    s->data=e;
+    s->next=p->next;//p指针指向L结点，将s结点指向P所指的结点
+    p->next=s;//将p的next指针指向s结点
+}
 void test(){
     LinkList L;//声明一个指向单链表的指针，此刻没有创建一个结点
     InitList(L);//调用InitList函数，初始化一个空表
