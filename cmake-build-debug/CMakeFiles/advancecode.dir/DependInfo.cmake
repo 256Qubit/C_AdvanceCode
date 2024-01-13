a1e6ba09fee065a6c0509af1ef93384c2b8a6982
256Qubit <jmortyrick@gmail.com>
2024-01-13 10:34:41 +0800

    修正

---
+++ cmake-build-debug/CMakeFiles/advancecode.dir/DependInfo.cmake
@@ -0,0 +1,66 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/3/advancecode/2_3/Elemtype/main.c" "D:/3/advancecode/cmake-build-debug/CMakeFiles/advancecode.dir/2_3/Elemtype/main.c.obj"
+  "D:/3/advancecode/2_3/Initialization/main.c" "D:/3/advancecode/cmake-build-debug/CMakeFiles/advancecode.dir/2_3/Initialization/main.c.obj"
+  "D:/3/advancecode/2_3/LinearList/main.c" "D:/3/advancecode/cmake-build-debug/CMakeFiles/advancecode.dir/2_3/LinearList/main.c.obj"
+  "D:/3/advancecode/2_3/test(&)/main.c" "D:/3/advancecode/cmake-build-debug/CMakeFiles/advancecode.dir/2_3/test(&)/main.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  "../.idea"
+  "../2_3"
+  "../2_3/doubly linked list"
+  "../4"
+  "../5"
+  "../6"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/3/advancecode/2_3/Chain-Queue/main.cpp" "D:/3/advancecode/cmake-build-debug/CMakeFiles/advancecode.dir/2_3/Chain-Queue/main.cpp.obj"
+  "D:/3/advancecode/2_3/Dynamic(practice)/main.cpp" "D:/3/advancecode/cmake-build-debug/CMakeFiles/advancecode.dir/2_3/Dynamic(practice)/main.cpp.obj"
+  "D:/3/advancecode/2_3/Dynamic allocation of linear tables(C++)/main.cpp" "D:/3/advancecode/cmake-build-debug/CMakeFiles/advancecode.dir/2_3/Dynamic_allocation_of_linear_tables(C++)/main.cpp.obj"
+  "D:/3/advancecode/2_3/Initialization Sequence Table/main.cpp" "D:/3/advancecode/cmake-build-debug/CMakeFiles/advancecode.dir/2_3/Initialization_Sequence_Table/main.cpp.obj"
+  "D:/3/advancecode/2_3/Linked List/main.cpp" "D:/3/advancecode/cmake-build-debug/CMakeFiles/advancecode.dir/2_3/Linked_List/main.cpp.obj"
+  "D:/3/advancecode/2_3/Quene/main.cpp" "D:/3/advancecode/cmake-build-debug/CMakeFiles/advancecode.dir/2_3/Quene/main.cpp.obj"
+  "D:/3/advancecode/2_3/Single Linked List(test)/main.cpp" "D:/3/advancecode/cmake-build-debug/CMakeFiles/advancecode.dir/2_3/Single_Linked_List(test)/main.cpp.obj"
+  "D:/3/advancecode/2_3/Single Linker List(Test1)/main.cpp" "D:/3/advancecode/cmake-build-debug/CMakeFiles/advancecode.dir/2_3/Single_Linker_List(Test1)/main.cpp.obj"
+  "D:/3/advancecode/2_3/Single linked list/main.cpp" "D:/3/advancecode/cmake-build-debug/CMakeFiles/advancecode.dir/2_3/Single_linked_list/main.cpp.obj"
+  "D:/3/advancecode/2_3/Singly linked list creation/main.cpp" "D:/3/advancecode/cmake-build-debug/CMakeFiles/advancecode.dir/2_3/Singly_linked_list_creation/main.cpp.obj"
+  "D:/3/advancecode/2_3/Stack/main.cpp" "D:/3/advancecode/cmake-build-debug/CMakeFiles/advancecode.dir/2_3/Stack/main.cpp.obj"
+  "D:/3/advancecode/2_3/Static linear table/main.cpp" "D:/3/advancecode/cmake-build-debug/CMakeFiles/advancecode.dir/2_3/Static_linear_table/main.cpp.obj"
+  "D:/3/advancecode/2_3/doubly linked list/main.cpp" "D:/3/advancecode/cmake-build-debug/CMakeFiles/advancecode.dir/2_3/doubly_linked_list/main.cpp.obj"
+  "D:/3/advancecode/2_3/doubly linked list/static linked list.cpp" "D:/3/advancecode/cmake-build-debug/CMakeFiles/advancecode.dir/2_3/doubly_linked_list/static_linked_list.cpp.obj"
+  "D:/3/advancecode/2_3/parenthesis matching/main.cpp" "D:/3/advancecode/cmake-build-debug/CMakeFiles/advancecode.dir/2_3/parenthesis_matching/main.cpp.obj"
+  "D:/3/advancecode/2_3/uChain-Queue/main.cpp" "D:/3/advancecode/cmake-build-debug/CMakeFiles/advancecode.dir/2_3/uChain-Queue/main.cpp.obj"
+  "D:/3/advancecode/2_3/uChain-stack/main.cpp" "D:/3/advancecode/cmake-build-debug/CMakeFiles/advancecode.dir/2_3/uChain-stack/main.cpp.obj"
+  "D:/3/advancecode/4/StringNode/main.cpp" "D:/3/advancecode/cmake-build-debug/CMakeFiles/advancecode.dir/4/StringNode/main.cpp.obj"
+  "D:/3/advancecode/5/Traversal of binary tree/main.cpp" "D:/3/advancecode/cmake-build-debug/CMakeFiles/advancecode.dir/5/Traversal_of_binary_tree/main.cpp.obj"
+  "D:/3/advancecode/5/untitled/main.cpp" "D:/3/advancecode/cmake-build-debug/CMakeFiles/advancecode.dir/5/untitled/main.cpp.obj"
+  "D:/3/advancecode/6/TuoPu/main.cpp" "D:/3/advancecode/cmake-build-debug/CMakeFiles/advancecode.dir/6/TuoPu/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  "../.idea"
+  "../2_3"
+  "../2_3/doubly linked list"
+  "../4"
+  "../5"
+  "../6"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/advancecode.dir/cmake_clean.cmake
@@ -0,0 +1,36 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/advancecode.dir/2_3/Chain-Queue/main.cpp.obj"
+  "CMakeFiles/advancecode.dir/2_3/Dynamic(practice)/main.cpp.obj"
+  "CMakeFiles/advancecode.dir/2_3/Dynamic_allocation_of_linear_tables(C++)/main.cpp.obj"
+  "CMakeFiles/advancecode.dir/2_3/Elemtype/main.c.obj"
+  "CMakeFiles/advancecode.dir/2_3/Initialization/main.c.obj"
+  "CMakeFiles/advancecode.dir/2_3/Initialization_Sequence_Table/main.cpp.obj"
+  "CMakeFiles/advancecode.dir/2_3/LinearList/main.c.obj"
+  "CMakeFiles/advancecode.dir/2_3/Linked_List/main.cpp.obj"
+  "CMakeFiles/advancecode.dir/2_3/Quene/main.cpp.obj"
+  "CMakeFiles/advancecode.dir/2_3/Single_Linked_List(test)/main.cpp.obj"
+  "CMakeFiles/advancecode.dir/2_3/Single_Linker_List(Test1)/main.cpp.obj"
+  "CMakeFiles/advancecode.dir/2_3/Single_linked_list/main.cpp.obj"
+  "CMakeFiles/advancecode.dir/2_3/Singly_linked_list_creation/main.cpp.obj"
+  "CMakeFiles/advancecode.dir/2_3/Stack/main.cpp.obj"
+  "CMakeFiles/advancecode.dir/2_3/Static_linear_table/main.cpp.obj"
+  "CMakeFiles/advancecode.dir/2_3/doubly_linked_list/main.cpp.obj"
+  "CMakeFiles/advancecode.dir/2_3/doubly_linked_list/static_linked_list.cpp.obj"
+  "CMakeFiles/advancecode.dir/2_3/parenthesis_matching/main.cpp.obj"
+  "CMakeFiles/advancecode.dir/2_3/test(&)/main.c.obj"
+  "CMakeFiles/advancecode.dir/2_3/uChain-Queue/main.cpp.obj"
+  "CMakeFiles/advancecode.dir/2_3/uChain-stack/main.cpp.obj"
+  "CMakeFiles/advancecode.dir/4/StringNode/main.cpp.obj"
+  "CMakeFiles/advancecode.dir/5/Traversal_of_binary_tree/main.cpp.obj"
+  "CMakeFiles/advancecode.dir/5/untitled/main.cpp.obj"
+  "CMakeFiles/advancecode.dir/6/TuoPu/main.cpp.obj"
+  "advancecode.exe"
+  "advancecode.exe.manifest"
+  "advancecode.pdb"
+  "libadvancecode.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/advancecode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
